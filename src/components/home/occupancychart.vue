3ba97ab9f2d93d2d3c88a8bf0d6b44bb5650d703
JeroldB26 <156853463+JeroldB26@users.noreply.github.com>
2024-04-12 16:53:04 -0400

    Add files via upload

---
+++ src/components/home/occupancychart.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="occupancy-chart">
+        <div v-for="(dataPoint, index) in data" :key="index" class="bar">
+            <div class="label">{{ dataPoint.label }}</div>
+            <div class="bar-wrapper">
+                <div class="bar-fill" :style="{ width: dataPoint.value + '%'}"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data:{
+            type: Array
+            required: true
+        }
+    }
+};
+</script>
+
+<style scoped>
+.occupany-chart {
+    display: flex;
+    flex-direction: column;
+}
+
+.bar {
+    margin-bottom: 10px;
+    display: flex;
+    align-items:center;
+}
+
+.label {
+    width: 100px;
+}
+
+.bar-wrapper {
+    width: 200px;
+    height: 200px;
+    background-color: lightgray;
+    border-radius: 5px;
+}
+
+.bar-fill{
+    height: 100%;
+    border-radius: 5px;
+    background-color: #007bff;
+}
+</style>
